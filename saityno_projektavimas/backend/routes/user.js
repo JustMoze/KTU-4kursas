f9c328025fd9b1ad9ad9adc53adacb9ba6108cc2
JustMoze <modestasrimeikis@gmail.com>
2020-09-17 17:35:52 +0300

    update user.js route

--- saityno_projektavimas/backend/routes/user.js
+++ saityno_projektavimas/backend/routes/user.js
@@ -37,7 +37,9 @@ router.post('/', async (req, res) => {
         return res.status(400).send(error.details[0].message);
     }
     try {
-        const user = new User({
+        let user = await User.findOne({email: req.body.email});
+        if(user) return res.status(400).send('User already registerd.');
+        user = new User({
             name: req.body.name,
             surname: req.body.surname,
             email: req.body.email,
