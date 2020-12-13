2f47cc058aaca47ff0409179ab7d68542e646062
JustMoze <modestasrimeikis@gmail.com>
2020-12-13 23:18:13 +0200

    connect to sentry

--- saityno_projektavimas/frontend/nba/pages/_app.js
+++ saityno_projektavimas/frontend/nba/pages/_app.js
@@ -6,6 +6,38 @@ import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
 import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
 import 'react-toastify/dist/ReactToastify.css';
 
+import * as Sentry from "@sentry/node";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+    dsn: "https://3936ef7f1f4a4807bdc0b2ebad31e3b2@o380079.ingest.sentry.io/5555193",
+    autoSessionTracking: true,
+    integrations: [
+      new Integrations.BrowserTracing(),
+    ],
+  
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+  });
+
+  const transaction = Sentry.startTransaction({
+    op: "test",
+    name: "My First Test Transaction",
+  });
+  
+  setTimeout(() => {
+    try {
+      // foo();
+      console.log('All good')
+    } catch (e) {
+      Sentry.captureException(e);
+    } finally {
+      transaction.finish();
+    }
+  }, 99);
+
+  
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
 }
